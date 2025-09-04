398a97d7869fe1db545d53fb4d12a10bc6e71c13
haroldevs <haroldthomasg18@gmail.com>
2025-09-04 13:31:26 -0500

    commit env

---
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -0,0 +1,32 @@
+import {  Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption{
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
+
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [RouterLink,RouterLinkActive],
+  templateUrl: './side-menu-options.component.html',
+})
+export class SideMenuOptionsComponent { 
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Los gifs mas populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search'
+    }
+  ]
+}
